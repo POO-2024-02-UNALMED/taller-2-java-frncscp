9d118e1a112d57e48768f1e4eec1d46bd5798513
frncscp <franciscocerenporto@gmail.com>
2024-11-11 22:31:10 -0500

    add: classes

---
+++ src/main/java/Auto.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static String cantidadCreados;
+	
+int cantidadAsientos(int ctd) {
+	return asientos.length;
+}
+
+String verificarIntegridad() {
+	
+	for (int i = 0; i < asientos.length; i++) {
+		if (asientos[i].registro != this.registro) {
+			return "Las piezas no son originales";
+		}
+	}
+	
+	if (this.motor.registro != this.registro) {
+		return "Las piezas no son originales";
+	}
+	
+	return "Auto Original";
+}
+
+}
