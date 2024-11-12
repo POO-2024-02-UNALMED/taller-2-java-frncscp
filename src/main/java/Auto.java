ddfad5faa7fd399a951e0468f1b380e8b48430e6
frncscp <franciscocerenporto@gmail.com>
2024-11-11 22:53:55 -0500

    fix: typo

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -22,7 +22,7 @@ int cantidadAsientos() {
 String verificarIntegridad() {
 	
 	for (int i = 0; i < asientos.length; i++) {
-		if if (asientos[i] != null && asientos[i].registro != this.registro) {
+		if (asientos[i] != null && asientos[i].registro != this.registro) {
 			return "Las piezas no son originales";
 		}
 	}
